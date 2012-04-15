22835f6bf38ecbc8a789bd92ff4b6f885ca330b0
Truls Michalsen <truls.michalsen@gmail.com>
2012-04-15 17:54:10 +0200

    Add password security to User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,12 +13,18 @@ require 'spec_helper'
 
 describe User do
 
-	before {@user = User.new(name: "Example User", email: "example@user.com")}
+	before {@user = User.new(name: "Example User", email: "example@user.com", 
+                           password: "foobar", password_confirmation: "foobar")}
 
 	subject {@user}
 
 	it {should respond_to(:name)}
 	it {should respond_to(:email)}
+  it {should respond_to(:password_digest)}
+  it {should respond_to(:password)}
+  it {should respond_to(:password_confirmation)}
+
+  it {should respond_to(:authenticate)}
 
   it {should be_valid}
 
@@ -30,6 +36,21 @@ describe User do
   describe "when email is not present" do
   	before {@user.email = "  "}
   	it {should_not be_valid}
+  end
+
+  describe "when password is not present" do
+    before {@user.password = @user.password_confirmation = " "}
+    it {should_not be_valid}
+  end
+
+  describe "when password doesn't match confirmation" do
+    before {@user.password = "foobar", @user.password_confirmation = "mismatch"}
+    it {should_not be_valid}
+  end
+
+  describe "when password confirmation is nil" do
+    before {@user.password = "foobar", @user.password_confirmation = nil}
+    it {should_not be_valid}
   end
 
   describe "when name is too long" do
@@ -66,4 +87,25 @@ describe User do
 
   	it {should_not be_valid}
   end
+
+  describe "return value of authenticate method" do
+  before { @user.save }
+  let(:found_user) { User.find_by_email(@user.email) }
+
+  describe "with valid password" do
+    it { should == found_user.authenticate(@user.password) }
+  end
+
+  describe "with invalid password" do
+    let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+    it { should_not == user_for_invalid_password }
+    specify { user_for_invalid_password.should be_false }
+  end
+
+  describe "with a password that is too short" do
+    before {@user.password = @user.password_confirmation = "a"*5}
+    it {should_not be_valid}
+  end
+end
 end
