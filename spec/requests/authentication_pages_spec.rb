5c1039b4cd46c18e142e06abc53f667bd95d3f3c
Truls Michalsen <truls.michalsen@gmail.com>
2012-04-21 19:33:11 +0200

    Add an edit user functionality

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -31,17 +31,56 @@ describe "Authentication" do
         click_button "Sign in"
       end
 
-      it {should have_selector('a', text: "Sign out")}
-      it {should have_link('Sign out')}
+      it { should have_selector('title', text: user.name) }
+      it { should have_link('Profile',  href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
+      it { should have_link('Sign out', href: signout_path) }
+      it { should_not have_link('Sign in', href: signin_path) }
     end
   end
 
+  # Denne har jeg skrevet selv
   describe "sign out" do
     before {visit signout_path}
 
     it {should have_selector('h1', text: "project2")}
     it {should have_link('Sign in')}
     it {should_not have_selector('a', text: "Account")}
+  end
 
+  describe "authorization" do
+
+    describe "for non signed in users" do
+      let(:user) {FactoryGirl.create(:user)}
+
+      describe "in the users controller" do
+        
+        describe "visiting the edit page" do
+          before {visit edit_user_path(user)}
+          it {should have_selector('title', text: "Sign in")}
+        end
+
+        describe "submitting the update action" do
+          before {put user_path(user)}
+          specify {response.should redirect_to(signin_path)}
+        end
+      end
+    end
+
+    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+      before { sign_in user }
+
+      describe "visiting Users#edit page" do
+        before { visit edit_user_path(wrong_user) }
+        it { should_not have_selector('title', text: full_title('Edit user')) }
+      end
+
+      describe "submitting a PUT request to the Users#update action" do
+        before { put user_path(wrong_user) }
+        specify { response.should redirect_to(root_path) }
+      end
+    end  
   end
 end
