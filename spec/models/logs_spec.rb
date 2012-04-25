d6cf359f249bcd6e12ede4f16cbbe75ce31bb290
Truls Michalsen <truls.michalsen@gmail.com>
2012-04-25 22:38:36 +0200

    Add some user model tests

--- spec/models/logs_spec.rb
+++ spec/models/logs_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Logs do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.create(:user) }
+  let(:project) { FactoryGirl.create(:project) }
+	before {@log = user.projects.logs.build(name: "Sleep",
+																					description: "How much sleep")}
+
+	subject {@log}
+
+	it {should respond_to(:name)}
+	it {should respond_to(:description)}
+	it {should respond_to(:project_id)}
+
+
 end
