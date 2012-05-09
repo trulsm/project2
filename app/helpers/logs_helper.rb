e594aea0dce5dc34e5706e73a83e3564965430d5
Truls Michalsen <truls.michalsen@gmail.com>
2012-05-09 20:22:57 +0200

    Add a sum and mean value line in project show page

--- app/helpers/logs_helper.rb
+++ app/helpers/logs_helper.rb
@@ -1,2 +1,27 @@
 module LogsHelper
+
+	def akkumulert(project)
+		if project.logs.any?
+			akkumulert = 0
+			project.logs.each do |log|
+				akkumulert = akkumulert + log.data_reg
+			end
+			return akkumulert.to_f
+		else
+			return "No logs recorded"
+		end
+	end
+
+	def mean_value(project)
+		if project.logs.any?
+			sum = 0
+			project.logs.each do |log|
+				sum = sum + log.data_reg
+			end
+			return (sum/project.logs.count).to_f
+		else
+			return "No logs recorded"
+		end
+	end
+
 end
